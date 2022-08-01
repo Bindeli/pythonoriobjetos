ca6b9a3e3f483329330b141dad79a0b3e0817c7e
LucasBindeli <lucasb2204@hotmail.com>
2022-08-01 11:58:02 -0300

    resumonil2

---
+++ resumonil/resumo2.py
@@ -0,0 +1,6 @@
+grupo = input('Qual é o seu grupo [A ou B ou C]? R: ')
+
+if not grupo == 'B' and not grupo == 'b':
+   print('Seu grupo não foi selecionado, O grupo B foi o ganhador.')
+else:
+   print('Parabéns, seu grupo foi selecionado!!!')
